5358a6de35ac562960c6035dcdcd8cf7293b095f
Eduardo Stamm <edstamm29@gmail.com>
2021-05-01 13:13:55 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// ----------Recovered from: https://stackoverflow.com/questions/1173194/select-all-div-text-with-single-mouse-click/1173319------------------
+function selectText(containerid) {
+    if (document.selection) { // IE
+        var range = document.body.createTextRange();
+        range.moveToElementText(document.getElementById(containerid));
+        range.select();
+    } else if (window.getSelection) {
+        var range = document.createRange();
+        range.selectNode(document.getElementById(containerid));
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+    }
+}
